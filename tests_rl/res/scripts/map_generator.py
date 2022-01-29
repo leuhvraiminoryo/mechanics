aa0050ebbddd835160052d19784b19847a228a1b
DandiFFH <73031809+DandiFFH@users.noreply.github.com>
2022-01-29 17:45:04 +0100

    hiérarchie des templates
    Extraction des templates dans un dictionary rooms dont la clé est le directory où ils se trouvent et donc le type de salle qu'il s'agit.
    Ajout de templates: corridors et une room de spawn
    Début de la forme que prennent les classes (Salle, Door)

--- tests_rl/res/scripts/map_generator.py
+++ tests_rl/res/scripts/map_generator.py
@@ -3,20 +3,34 @@ import os
 correspondings_sides = {"u":"d","d":"u","r":"l","l":"r"}
 
 def extractRooms():
-    rooms = []
+    rooms = {}
 
     directory = 'tests_rl/res/templates'
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f):
+
+    for element in os.listdir(directory):
+        e = os.path.join(directory, element)
+
+        if os.path.isdir(e):
+            liste = []
+            for templ in os.listdir(e):
+                room = []
+                nut = os.path.join(e,templ)
+                with open(nut) as file:
+                    lines = file.readlines()
+                    for line in lines:
+                        room.append(line.replace('\n',''))
+                liste.append(room)
+            rooms[element] = liste
+
+        if os.path.isfile(e):
             room = []
-            with open(f) as file:
+            with open(e) as file:
                 lines = file.readlines()
                 for line in lines:
                     room.append(line.replace('\n',''))
-            rooms.append(room)
+            rooms[element] = room
+
     return rooms
 
 rooms = extractRooms()
-for i in rooms:
-    print(i)
+print(rooms)
--- tests_rl/res/scripts/zones.py
+++ tests_rl/res/scripts/zones.py
@@ -1,6 +1,19 @@
 class Salle:
-    def __init__(self, id, map, linked):
-        self.id = id
+    id = 0
+    def __init__(self, map):
+        self.id = id 
         self.map = map
-        self.link = linked
-        
+        self.link = {}
+        Salle.id += 1
+
+    def link(self,id):
+        pass
+
+class Door:
+    id = 0
+    def __init__(self, pos, porte_linked):
+        self.pos = pos
+        self.link = porte_linked
+        Door.id += 1
+    
+
