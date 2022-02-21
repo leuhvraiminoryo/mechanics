0f49a94d2dace0d3094b70016c2483c688a7b41b
leuhvraiminoryo <50586611+leuhvraiminoryo@users.noreply.github.com>
2022-02-21 19:40:12 +0100

    Update zones.py (encore)

--- tests_rl/res/scripts/zones.py
+++ tests_rl/res/scripts/zones.py
@@ -1,9 +1,14 @@
+class Map:
+    def __init__(self,rooms=[],doors=[]):
+        self.rooms = rooms
+        self.doors = doors
+
 class Salle:
     id = 0
     def __init__(self, map, doors=[]):
         self.id = Salle.id 
         self.map = map
-        self.doors = doors #list of all doors in Da Room
+        self.doors = doors #list of all ids of doors in Da Room
         Salle.id += 1
 
     def link(self,id):
@@ -14,7 +19,7 @@ class Door:
     def __init__(self, pos, porte_linked=None):
         self.id = Door.id
         self.pos = pos
-        self.link = porte_linked #Da Related Door
+        self.link = porte_linked #Da Related Door's id
         Door.id += 1
     
 
