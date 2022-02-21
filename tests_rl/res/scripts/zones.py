30b04248ed013dbb383ffafb4caeef7994be4476
leuhvraiminoryo <50586611+leuhvraiminoryo@users.noreply.github.com>
2022-02-21 19:36:09 +0100

    Update zones.py

--- tests_rl/res/scripts/zones.py
+++ tests_rl/res/scripts/zones.py
@@ -1,19 +1,20 @@
 class Salle:
     id = 0
-    def __init__(self, map):
-        self.id = id 
+    def __init__(self, map, doors=[]):
+        self.id = Salle.id 
         self.map = map
-        self.link = {}
+        self.doors = doors #list of all doors in Da Room
         Salle.id += 1
 
     def link(self,id):
-        pass
+        self.link.append(id)
 
 class Door:
     id = 0
-    def __init__(self, pos, porte_linked):
+    def __init__(self, pos, porte_linked=None):
+        self.id = Door.id
         self.pos = pos
-        self.link = porte_linked
+        self.link = porte_linked #Da Related Door
         Door.id += 1
     
 
